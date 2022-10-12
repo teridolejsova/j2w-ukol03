f6c8cf7af27676f559cd22cff7d5aa19ddca498e
Tereza Dolejšová <terdoe>
2022-10-12 19:51:29 +0200

    Hotovo

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,35 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+ private List<Vizitka> udajeVizitka= List.of(
+         new Vizitka("Jitka Dolejšová","Domika s r.o.","Dačického 7", "Praha 4, 140 00","jitkadolejsova@seznam.cz","737830066","www.domika.name"),
+         new Vizitka("Zuzana Tučanová","IKEM","Vídeňská 1958","14021 Praha 4","kardiochirurgie@ikem.cz","+420 776292211","www.ikem.cz"),
+         new Vizitka("Dominik Dolejš","Planeo"," U Sanitasu 1621","251 01 Říčany","dominikdolejs@planeo.cz","+420 323 204 584","www.planeo.cz"),
+         new Vizitka("Radomír Dolejš","Advokáti s r.o.","Dačického 7","Praha 4, 140 00",null,"+420 775628064",null)
+          );
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+  public ModelAndView seznam() {
+      ModelAndView modelAndView = new ModelAndView("/seznam");
+      modelAndView.addObject("seznamVizitek",udajeVizitka);
+    return modelAndView;
   }
 
-  @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  @GetMapping("/detail/{id}")
+  public ModelAndView detail(@PathVariable int id) {
+      ModelAndView modelAndView = new ModelAndView("/detail");
+      modelAndView.addObject("jedenUdaj",udajeVizitka.get(id));
+      return modelAndView;
   }
 }
